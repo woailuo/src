dce92d533916031ff8ed1687d7929edb5d26b427
woailuo <ki.tan@hotmail.com>
2015-05-12 11:42:45 +0900

    update tut1.ml

--- tut1.ml
+++ tut1.ml
@@ -1,7 +1,6 @@
 open Cil
 module E = Errormsg
 
-
 let rec tut1FixInstrs ins : bool =
   match ins with
     i :: rest -> (fixinstr i) && (tut1FixInstrs rest)
@@ -23,20 +22,43 @@ and isPointer c =
   | AlignOfE _ | UnOp _  | BinOp  _  | Question _
   | CastE _  | AddrOf _  | AddrOfLabel _  | StartOf _ -> false
 
-and findFreePointer pr =
+and findFreeP (pr:exp) (b:block) =
+  let stms = b.stmts in (* stmt list*)
+  match stms.skind with
+    Instr il -> fixinstrs il
+  |  If(_, tb,fb,_) ->
 
+and fixinstrs instrs  pr =
+    match ins with
+    i :: rest -> (fixInstr  i) ^  (fixinstrs rest)
+  | [] -> false
 
-and  fixinstr (i: instr) =
+and  fixinstr (i: instr)  pr =
   match i with
   | Set((Var vi, NoOffset), _, loc) -> true
   | Call (_, exp, _ ,location)   ->  fixcallnone exp
   | Asm _-> false
   |  _ -> false
 
+and raiseNullExExp (e : exp) (v : lval) : bool =
+(* returns true iff evaluating e raises a NullEx cased by v *)
+and raiseNullExInstr (i : instr) (v : lval) :  bool =
+(* returns true iff executing i raises a NullEx caused by v *)
+    match e with
+    Set(l, e, _) ->raiseNullExLval l v || raiseNullExExp e v
+    | _ -> failwith "implement here."
+and raiseNullExInstrs (is : instr list) ( v : lval) : bool =
+and raiseNullExStmt (s : stmt) (v : lval) : bool =
+  match s.skind with
+  | If(e, b1, b2, _) ->
+     raiseNullExExp e v || (raiseNullExBlock b1 v && raiseNullExBlock b2 v)
+  | Break _ -> false
+  | Loop(b, _, _, _) -> raiseNullExBlock b v
+
 and tut1FixStmt (s : stmt) : unit =
   match s.skind with
-  | Instr il ->
-    if  (  tut1FixInstrs il ) then (print_string " \n test \n";)
+  (* | Instr il -> *)
+
   | If(pr,tb,fb,_) ->
      let b1 = isPointer  pr in
      match b1 with
