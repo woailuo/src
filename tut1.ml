f25b7771363d5a5cabe454706ed6b3338a7a70f4
woailuo <ki.tan@hotmail.com>
2015-05-14 11:01:04 +0900

    update

--- tut1.ml
+++ tut1.ml
@@ -1,94 +1,149 @@
 open Cil
 module E = Errormsg
 
-let rec tut1FixInstrs ins : bool =
-  match ins with
-    i :: rest -> (fixinstr i) && (tut1FixInstrs rest)
-  | [] -> true
+(* and raiseNullExExp (e : exp) (v : lval) : bool = *)
+(* (\* returns true iff evaluating e raises a NullEx cased by v *\) *)
+(* and raiseNullExInstr (i : instr) (v : lval) :  bool = *)
+(* (\* returns true iff executing i raises a NullEx caused by v *\) *)
+(*     match e with *)
+(*     Set(l, e, _) ->raiseNullExLval l v || raiseNullExExp e v *)
+(*     | _ -> failwith "implement here." *)
+(* and raiseNullExInstrs (is : instr list) ( v : lval) : bool = *)
+(* and raiseNullExStmt (s : stmt) (v : lval) : bool = *)
+(*   match s.skind with *)
+(*   | If(e, b1, b2, _) -> *)
+(*      raiseNullExExp e v || (raiseNullExBlock b1 v && raiseNullExBlock b2 v) *)
+(*   | Break _ -> false *)
+(*   | Loop(b, _, _, _) -> raiseNullExBlock b v *)
 
-and fixcallnone c =
-  match c with
-  | Lval a -> true
-  | Const _
-  | SizeOf _  | SizeOfE _ | SizeOfStr _  | AlignOf _
-  | AlignOfE _ | UnOp _  | BinOp  _  | Question _
-  | CastE _  | AddrOf _  | AddrOfLabel _  | StartOf _ -> false
+let isFree = ref false
 
-and isPointer c =
-  match c with
-  | Lval a -> true
-  | Const _
-  | SizeOf _  | SizeOfE _ | SizeOfStr _  | AlignOf _
-  | AlignOfE _ | UnOp _  | BinOp  _  | Question _
-  | CastE _  | AddrOf _  | AddrOfLabel _  | StartOf _ -> false
+let rec callFree pname  exp  =
+  match exp with
+  | Lval a ->
+     (
+       match a with
+         (l, offset) ->
+         (
+           match l with
+             Var a  when a.vname = "free" -> print_string " free\n"; true
+           | Var b -> (print_string b.vname); print_string " call func\n"; false
+           | Mem _ -> print_string "  call mem\n"; false
+         )
+     )
+  | Const _ -> print_string " cconst\n"; false
+  | SizeOf _ -> print_string " csizeof\n"; false
+  | SizeOfE _ -> print_string " csizeofe\n"; false
+  | SizeOfStr _ -> print_string " csizeofstr\n"; false
+  | AlignOf _ -> print_string " calignof\n"; false
+  | AlignOfE _ -> print_string " calignofe\n"; false
+  | UnOp _ -> print_string " cunop\n"; false
+  | BinOp  _ -> print_string " cbinop\n"; false
+  | Question _ -> print_string " cquestion\n"; false
+  | CastE _ -> print_string " ccaste\n"; false
+  | AddrOf _ -> print_string " caddof\n"; false
+  | AddrOfLabel _ -> print_string " caddroflabel\n"; false
+  | StartOf _ -> print_string " cstartof\n"; false
 
-and findFreeP (pr:exp) (b:block) =
-  let stms = b.stmts in (* stmt list*)
-  match stms.skind with
-    Instr il -> fixinstrs il
-  |  If(_, tb,fb,_) ->
+let rec findFreeInstr instr pname =
+  match instr with
+  | Set _ -> print_string " Set\n"; false
+  | Call (_,exp,exps,location) -> (callFree pname exp )
+  | Asm _ -> print_string " asm\n"; false
 
-and fixinstrs instrs  pr =
-    match ins with
-    i :: rest -> (fixInstr  i) ^  (fixinstrs rest)
+and  findFreeInstrs instrs pname =
+  match instrs with
+    i :: rest -> let b1 =  (findFreeInstr i pname) in
+                 let b2 = (findFreeInstrs rest pname) in
+                 b1 || b2
   | [] -> false
 
-and  fixinstr (i: instr)  pr =
-  match i with
-  | Set((Var vi, NoOffset), _, loc) -> true
-  | Call (_, exp, _ ,location)   ->  fixcallnone exp
-  | Asm _-> false
-  |  _ -> false
+and  findFreeFun pname stm =
+  match stm.skind with
+  | Instr il -> let b = (findFreeInstrs il pname) in
+                isFree := b
+  | Return _ -> print_string " return\n"
+  | Goto _ -> print_string " Goto\n"
+  | ComputedGoto _ -> print_string "ComputedGoto\n"
+  | Break _ -> print_string " Break\n"
+  | Continue _ -> print_string " Continue\n"
+  | If(pr,tb,fb,_) -> hasFree tb pr
+  | Switch(_,b,_,_) -> print_string " switch \n"
+  | Loop(b,_,_,_) -> print_string " loop\n"
+  | Block b -> print_string " Block\n"
+  | TryFinally(b1, b2, _) -> print_string " TryFinally\n"
+  | TryExcept(b1,_,b2,_) -> print_string " TryExcept\n"
 
-and raiseNullExExp (e : exp) (v : lval) : bool =
-(* returns true iff evaluating e raises a NullEx cased by v *)
-and raiseNullExInstr (i : instr) (v : lval) :  bool =
-(* returns true iff executing i raises a NullEx caused by v *)
-    match e with
-    Set(l, e, _) ->raiseNullExLval l v || raiseNullExExp e v
-    | _ -> failwith "implement here."
-and raiseNullExInstrs (is : instr list) ( v : lval) : bool =
-and raiseNullExStmt (s : stmt) (v : lval) : bool =
+and hasFree (blk:block) pname =
+  List.iter (findFreeFun pname) blk.bstmts
+
+and  isPointer (e:exp) =
+  match e with
+  | Lval a -> ( match a with
+                  (lhost, offset) -> match lhost with
+                                       Var info -> print_string info.vname;
+                                                   ( match info.vtype with
+                                                       TVoid _   -> print_string " tvoid\n"; false
+                                                     | TInt _  -> print_string " tint\n"; false
+                                                     | TFloat  _ -> print_string " tfloat\n"; false
+                                                     | TPtr _ -> true
+                                                     |  TArray _ -> print_string " tvoid\n"; false
+                                                     |  TFun _ -> print_string " tvoid\n"; false
+                                                     | TNamed _  -> print_string " tnamed\n"; false
+                                                     | TComp _ -> print_string " tcomp\n"; false
+                                                     | TEnum _ -> print_string " tenum\n"; false
+                                                     | TBuiltin_va_list  _ -> print_string " tbuiltin_va_list\n"; false
+                                                   )
+
+                                   | Mem exp -> isPointer exp )
+  | Const _ -> print_string " Const\n"; false
+  | SizeOf _ -> print_string " sizeof\n"; false
+  | SizeOfE _ -> print_string "sizeofe\n"; false
+  | SizeOfStr _-> print_string "sizeofstr\n"; false
+  | AlignOf _ -> print_string " alignof\n"; false
+  | AlignOfE _ -> print_string "alignofe\n"; false
+  | UnOp _ -> print_string "unop\n"; false
+  | BinOp  _ -> print_string "binop\n"; false
+  | Question _  -> print_string "question\n"; false
+  | CastE _ -> print_string " casee\n"; false
+  | AddrOf _ -> print_string " addof\n"; false
+  | AddrOfLabel _ -> print_string " addroflabel\n"; false
+  | StartOf _ -> print_string " startof\n"; false
+
+and analyStmts (s : stmt) : unit =
   match s.skind with
-  | If(e, b1, b2, _) ->
-     raiseNullExExp e v || (raiseNullExBlock b1 v && raiseNullExBlock b2 v)
-  | Break _ -> false
-  | Loop(b, _, _, _) -> raiseNullExBlock b v
+  | Instr il -> print_string " instr\n";
+  | Return _ -> print_string " return\n"
+  | Goto _ -> print_string " Goto\n"
+  | ComputedGoto _ -> print_string "ComputedGoto\n"
+  | Break _ -> print_string " Break\n"
+  | Continue _ -> print_string " Continue\n"
+  | If(pr,tb,fb,_) -> let flag = (isPointer pr) in  (* if(exp, block ,block, location)*)
+                      (
+                        match flag with
+                          false -> print_string " not pointer \n"
+                        | true ->  (hasFree tb pr);
+                                   let b = !isFree in
+                                   match b with
+                                     true -> analyBlock tb
+                                   | false -> print_string " isFree is false\n"
+                      )
 
-and tut1FixStmt (s : stmt) : unit =
-  match s.skind with
-  (* | Instr il -> *)
+  | Switch(_,b,_,_) -> print_string " switch\n "
+  | Loop(b,_,_,_) -> print_string " loop\n"
+  | Block b -> print_string " Block\n"
+  | TryFinally(b1, b2, _) -> print_string " TryFinally\n"
+  | TryExcept(b1,_,b2,_) -> print_string " TryExcept\n"
 
-  | If(pr,tb,fb,_) ->
-     let b1 = isPointer  pr in
-     match b1 with
-       true ->
-     | false -> tut1FixBlock tb
+and analyBlock (b : block) : unit = List.iter analyStmts b.bstmts
 
-
-  | Switch(_,b,_,_) ->
-    tut1FixBlock b
-  | Loop(b,_,_,_) ->
-    tut1FixBlock b
-  | Block b ->
-    tut1FixBlock b
-  | TryFinally(b1, b2, _) ->
-    tut1FixBlock b1;
-    tut1FixBlock b2
-  | TryExcept(b1,_,b2,_) ->
-    tut1FixBlock b1;
-    tut1FixBlock b2
-
-  | _ -> ()
-
-and tut1FixBlock (b : block) : unit = List.iter tut1FixStmt b.bstmts
-
-let tut1FixFunction (fd : fundec) : unit = tut1FixBlock fd.sbody
+let analyFuns (func : fundec) : unit = analyBlock func.sbody
 
 let tut1 (f : file) : unit =
- ( List.iter (fun g ->
-    match g with
-    | GFun (fd, loc) ->
-       tut1FixFunction fd
-    | _ -> () )
-  f.globals )
+  ( List.iter
+      (fun g -> match g with
+                        | GFun (func, loc) -> analyFuns func
+                        | _ -> ()
+      )
+      f.globals (*global list :  functions*)
+  )
